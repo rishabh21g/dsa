c13c65e1639fb3e729e6d7ea748cd370aafb7b32
rishabh21g <biasedengineer@gmail.com>
2025-05-29 00:24:56 +0530

    remove duplicates

---
+++ array/removeDup.js
@@ -0,0 +1,31 @@
+let arr = [12, 14, 7, 55, 3, 9, 9, 55, 1, 1, 1, 14];
+let arr1 = [1, 2, 4, 8, 9];
+
+let uniqueArr = [...new Set(arr)]; // shortcut
+// console.log(uniqueArr)
+
+// Brute force
+function delDup(arr) {
+  let uniqueArr = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    uniqueArr.add(arr[i]);
+  }
+  return [...uniqueArr];
+}
+
+// console.log(delDup(arr));
+
+// by hashing
+function remDupByHash(arr) {
+  let uniqueArr = [];
+  let map = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (!map[arr[i]]) {
+      uniqueArr.push(arr[i]);
+      map[arr[i]] = true;
+    }
+  }
+  return uniqueArr;
+}
+
+console.log(remDupByHash(arr))
